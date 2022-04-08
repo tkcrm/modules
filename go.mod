422ebabfcdf148a80e7a728e593da0d6c0c77558
sxwebdev <sxwebdev@gmail.com>
2022-04-08 23:47:34 +0300

    update utils

--- go.mod
+++ go.mod
@@ -3,15 +3,17 @@ module github.com/tkcrm/modules
 go 1.18
 
 require (
-	github.com/nats-io/nats.go v1.13.1-0.20220308171302-2f2f6968e98d
+	github.com/nats-io/nats.go v1.14.0
 	go.uber.org/zap v1.21.0
 )
 
 require (
+	github.com/goccy/go-json v0.9.6
 	github.com/nats-io/nats-server/v2 v2.7.4 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.7.0 // indirect
-	golang.org/x/crypto v0.0.0-20220210151621-f4118a5b28e2 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921 // indirect
+	google.golang.org/protobuf v1.28.0
 )
--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,29 @@
 package utils
 
+import (
+	"math"
+
+	"github.com/goccy/go-json"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+func Pointer[T any](v T) *T {
+	return &v
+}
+
+func Round(v float64, n float64) float64 {
+	return math.Round(v*n) / n
+}
+
+func GetDefaultBool(v, d bool) bool {
+	if v == false {
+		v = d
+	}
+	return v
+}
+
 func GetDefaultString(v, d string) string {
 	if v == "" {
 		v = d
@@ -21,4 +45,51 @@ func ExistInArray[T comparable](arr []T, value T) bool {
 		}
 	}
 	return false
+}
+
+func ToProto(src interface{}, dst protoreflect.ProtoMessage) error {
+
+	result, err := json.Marshal(src)
+	if err != nil {
+		return err
+	}
+
+	if err := protojson.Unmarshal(result, dst); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func FromProto[TDst any](src protoreflect.ProtoMessage) (TDst, error) {
+
+	m := protojson.MarshalOptions{
+		UseProtoNames: true,
+	}
+
+	var dst TDst
+	bs, err := m.Marshal(src)
+	if err != nil {
+		return dst, err
+	}
+
+	if err := json.Unmarshal(bs, &dst); err != nil {
+		return dst, err
+	}
+
+	return dst, nil
+}
+
+func ToModel(src interface{}, dst interface{}) error {
+
+	result, err := json.Marshal(src)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(result, dst); err != nil {
+		return err
+	}
+
+	return nil
 }
