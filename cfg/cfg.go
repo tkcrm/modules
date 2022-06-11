2e1f7f963c0bac72bf9eaa58c07e8fd86176bebe
sxwebdev <sxwebdev@gmail.com>
2022-06-11 17:27:36 +0300

    update config

--- cfg/cfg.go
+++ cfg/cfg.go
@@ -27,21 +27,29 @@ type IConfig interface {
 //	if err := cfg.LoadConfig(&config); err != nil {
 //		log.Fatalf("could not load configuration: %v", err)
 //	}
-func LoadConfig(cfg IConfig) error {
+func LoadConfig(cfg IConfig, opts ...Option) error {
 
 	if reflect.ValueOf(cfg).Kind() != reflect.Ptr {
 		return fmt.Errorf("config variable must be a pointer")
 	}
 
-	pwdDir, err := os.Getwd()
-	if err != nil {
-		return err
+	options := Options{}
+	for _, opt := range opts {
+		opt(&options)
+	}
+
+	if options.EnvPath == "" {
+		pwdDir, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		options.EnvPath = pwdDir
 	}
 
 	aconf := aconfig.Config{
 		AllowUnknownFields: true,
 		SkipFlags:          true,
-		Files:              []string{path.Join(pwdDir, ".env")},
+		Files:              []string{path.Join(options.EnvPath, ".env")},
 		FileDecoders: map[string]aconfig.FileDecoder{
 			".env": aconfigdotenv.New(),
 		},
