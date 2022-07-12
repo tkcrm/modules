730a5b22590cc1a84f5494db5aa48008dd76c49e
sxwebdev <sxwebdev@gmail.com>
2022-07-12 20:55:51 +0300

    add pagination util

---
+++ db/dbutils/pagination.go
@@ -0,0 +1,46 @@
+package dbutils
+
+import "fmt"
+
+type Option func(*Options)
+
+type Options struct {
+	MaxLimit uint64
+}
+
+func WithMaxLimit(v uint64) Option {
+	return func(o *Options) {
+		o.MaxLimit = v
+	}
+}
+
+func Pagination(page, records *uint64, opts ...Option) (limit, offset uint64, err error) {
+
+	options := Options{
+		MaxLimit: 100,
+	}
+	for _, opt := range opts {
+		opt(&options)
+	}
+
+	limit = 100
+	offset = 0
+
+	if records != nil {
+		limit = *records
+	}
+
+	if page != nil && *page != 0 {
+		offset = (*page - 1) * limit
+	}
+
+	if limit < 1 {
+		return 0, 0, fmt.Errorf("pagination error: records cannot be less than 1")
+	}
+
+	if limit > options.MaxLimit {
+		return 0, 0, fmt.Errorf("pagination error: records cannot be greater than %d", options.MaxLimit)
+	}
+
+	return
+}
---
+++ db/dbutils/pagination_test.go
@@ -0,0 +1,18 @@
+package dbutils_test
+
+import (
+	"testing"
+
+	"github.com/tkcrm/modules/db/dbutils"
+)
+
+func Test_Pagination(t *testing.T) {
+
+	page := uint64(1)
+	records := uint64(1)
+
+	_, _, err := dbutils.Pagination(&page, &records, dbutils.WithMaxLimit(101))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
