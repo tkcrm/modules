86ec5895d641db1cdb8b8816446816e177fc6d2b
sxwebdev <sxwebdev@gmail.com>
2024-04-22 19:04:40 +0300

    fix for pgtypeutils encode text

--- pkg/db/dbutils/pgtypeutils/text.go
+++ pkg/db/dbutils/pgtypeutils/text.go
@@ -4,12 +4,14 @@ import "github.com/jackc/pgx/v5/pgtype"
 
 func EncodeText(value *string) pgtype.Text {
 	v := ""
+	var valid bool
 	if value != nil {
 		v = *value
+		valid = true
 	}
 	return pgtype.Text{
 		String: v,
-		Valid:  value != nil,
+		Valid:  valid,
 	}
 }
 
